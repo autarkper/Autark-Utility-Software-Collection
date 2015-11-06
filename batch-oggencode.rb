ee4999de56648324b9d908839a79414e1e07fe64
Per Ångström <github@autark.se>
2015-11-06 11:06:04 +0100

    batch-oggencode.rb: don't print "Successfully processed 0 files"

--- batch-oggencode.rb
+++ batch-oggencode.rb
@@ -545,7 +545,9 @@ end
 if (@@exists > 0)
     puts "\n#{File.basename($0)}: Not overwritten: #{@@exists}"
 end
-puts "\n#{File.basename($0)}: Sucessfully processed #{@@converted} file#{if (@@converted != 1) then 's' end}#{if (@@dry_run) then ' (DRY RUN)' end}."
+if (@@converted > 0)
+    puts "\n#{File.basename($0)}: Sucessfully processed #{@@converted} file#{if (@@converted != 1) then 's' end}#{if (@@dry_run) then ' (DRY RUN)' end}."
+end
 
 failure_count = @@jobs_total - @@jobs_ok;
 if (failure_count > 0)
